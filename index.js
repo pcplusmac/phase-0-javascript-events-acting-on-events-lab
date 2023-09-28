f9bd548ce3bc79e4fc81a19cc4fdd5a916375301
el-sam <samurai.h@yahoo.com>
2023-09-28 12:34:18 +0800

    define funtion: move to the left; and the function move to right

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+// Define funciton move to left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+//   Define functio move to right
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+//   Add event listener
+
